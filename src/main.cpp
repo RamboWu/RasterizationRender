873de9972c4f0c5c7b6e9503240198178527523a
吴培峰 <wupeifeng@7fgame.com>
2016-03-09 10:38:32 +0800

    支持了 在一个二维数组里保存颜色，然后输出到屏幕上。

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,12 @@
 
 #endif
 
+#include <stdlib.h>
 #include <GL/glfw3.h>
+
+GLubyte	*PixelData = NULL;          // Pointer to bits
+const int image_width = 100;
+const int image_height = 100;
 
 int main(void)
 {
@@ -28,9 +33,22 @@ int main(void)
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
+	PixelData = (GLubyte*)malloc(image_width * image_height * 3 * sizeof(GLubyte));
+
+	for (int i = 0; i < image_height; i++)
+		for (int j = 0; j < image_width; j++)
+		{
+		PixelData[(i*image_width + j) * 3] = 0;
+		PixelData[(i*image_width + j) * 3 + 1] = 0;
+		PixelData[(i*image_width + j) * 3 + 2] = 0;
+		}
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 		/* Draw a triangle */
 		glBegin(GL_TRIANGLES);
 
@@ -44,6 +62,8 @@ int main(void)
 		glVertex3f(1.0, -1.0, 0.0);
 
 		glEnd();
+
+		glDrawPixels(image_width, image_height, GL_RGB, GL_UNSIGNED_BYTE, PixelData);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
